578c4f34177a8b9082fd015900677272473672bf
Ruthira Moorthi <rudhram64@gmail.com>
2025-12-07 21:55:32 +0530

    Added Register Request

--- src/main/java/com/example/E_Commerce/Backend/Project/dto/RegisterRequest.java
+++ src/main/java/com/example/E_Commerce/Backend/Project/dto/RegisterRequest.java
@@ -1,4 +1,8 @@
 package com.example.E_Commerce.Backend.Project.dto;
 
-public class RegisterRequest {
-}
+    public class RegisterRequest {
+        private String name;
+        private String email;
+        private String password;
+    }
+
