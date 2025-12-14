e8c174f793b5375bac5c43c526109edaa95655c4
Ruthira Moorthi <rudhram64@gmail.com>
2025-12-14 21:51:04 +0530

    Added Cart Controller

--- src/main/java/com/example/E_Commerce/Backend/Project/controller/CartController.java
+++ src/main/java/com/example/E_Commerce/Backend/Project/controller/CartController.java
@@ -1,4 +1,27 @@
 package com.example.E_Commerce.Backend.Project.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/add")
+    public String addToCart(@RequestBody CartRequest request) {
+        return cartService.addToCart(request);
+    }
+
+    @DeleteMapping("/remove/{itemId}")
+    public String removeItem(@PathVariable Long itemId) {
+        return cartService.removeItem(itemId);
+    }
+
+    @GetMapping("/")
+    public Cart viewCart() {
+        return cartService.viewCart();
+    }
 }
